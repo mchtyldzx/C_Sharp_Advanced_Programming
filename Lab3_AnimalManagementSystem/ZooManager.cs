4a124140af76f9088a719cd10f78b7b4f0245732
Mücahit Yıldız <mucahit._.yildiz@hotmail.com>
2025-05-13 15:42:35 +0200

    Update ZooManager.cs

--- Lab3_AnimalManagementSystem/ZooManager.cs
+++ Lab3_AnimalManagementSystem/ZooManager.cs
@@ -14,9 +14,42 @@ public class ZooManager
         {
             Herbivores.Add(herbivore);
         }
-        else if (animal is ICarnivore carnivore)
+        if (animal is ICarnivore carnivore)
         {
             Carnivores.Add(carnivore);
+        }
+    }
+
+    public void AddHerbivore(IHerbivore herbivore)
+    {
+        if (herbivore is Animal animal)
+        {
+            Animals.Add(animal);
+            Herbivores.Add(herbivore);
+        }
+    }
+
+    public void AddCarnivore(ICarnivore carnivore)
+    {
+        if (carnivore is Animal animal)
+        {
+            Animals.Add(animal);
+            Carnivores.Add(carnivore);
+        }
+    }
+
+    public void CopyAnimalsToDietLists()
+    {
+        foreach (var animal in Animals)
+        {
+            if (animal is IHerbivore herbivore && !Herbivores.Contains(herbivore))
+            {
+                Herbivores.Add(herbivore);
+            }
+            if (animal is ICarnivore carnivore && !Carnivores.Contains(carnivore))
+            {
+                Carnivores.Add(carnivore);
+            }
         }
     }
 
@@ -38,11 +71,50 @@ public class ZooManager
         }
     }
 
+    public static void FeedSingleHerbivore(IHerbivore herbivore)
+    {
+        herbivore.FindFood();
+        herbivore.EatPlant();
+    }
+
+    public static void FeedSingleCarnivore(ICarnivore carnivore)
+    {
+        carnivore.FindFood();
+        carnivore.EatMeat();
+    }
+
     public void DisplayAnimals()
     {
         foreach (var animal in Animals)
         {
             Console.WriteLine(animal);
         }
+    }
+
+    public void DisplayHerbivores()
+    {
+        foreach (var herbivore in Herbivores)
+        {
+            if (herbivore is Animal animal)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+
+    public void DisplayCarnivores()
+    {
+        foreach (var carnivore in Carnivores)
+        {
+            if (carnivore is Animal animal)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+
+    public Animal FindAnimalByName(string name)
+    {
+        return Animals.Find(a => a.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
     }
 }
